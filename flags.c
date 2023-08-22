f6edc13fb0da743a025deb5ea9fa0e0391846e46
Rasyohannesasfaw <136009927+Rasyohannesasfaw@users.noreply.github.com>
2023-08-23 00:39:44 +0300

    Update flags.c

--- flags.c
+++ flags.c
@@ -5,7 +5,7 @@
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
- * by Natenael and Yohannes
+ * By  Natenael Tamirat and Yohannes Asfawosen
  */
 int get_flags(const char *format, int *i)
 {
