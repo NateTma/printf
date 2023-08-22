970d5c82b68fb138646ed4859e060aab20dbdb38
Rasyohannesasfaw <136009927+Rasyohannesasfaw@users.noreply.github.com>
2023-08-23 00:41:08 +0300

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -11,7 +11,7 @@
  * @size: Size specifier
  *
  * Return: Number of chars printed.
- * by Natenael and Yohannes
+ * By  Natenael Tamirat and Yohannes Asfawosen
  */
 int handle_write_char(char c, char buffer[],
 	int flags, int width, int precision, int size)
